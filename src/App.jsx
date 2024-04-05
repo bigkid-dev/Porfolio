a0ff92cb0aac95f3f0a8715ca9ea7cad8161f69e
bigkid <egbeyemifisola777@gmail.com>
2024-04-05 10:29:03 +0100

    added elevation styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero />
       <About />
     </>
   );
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import aboutSvg from '../assets/about.svg';
 import SectionTitle from './SectionTitle';
+
 const About = () => {
   return (
     <section className='bg-white py-20' id='about'>
