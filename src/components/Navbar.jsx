31205108f67a21964556a824ecd08d72de9cbddb
bigkid <egbeyemifisola777@gmail.com>
2024-04-05 09:59:17 +0100

    portfolio initialization

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import { links } from "../data";
+const Navbar = () => {
+  return (
+    <nav className="bg-emerald-100">
+      <div className="align-element py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
+        <h2 className="text-3xl font-bold">Web</h2>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
